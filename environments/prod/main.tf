e11775f1ba2ee3f622af92a96c2018d06ee6f588
SWETA_RAJ <swetaraj@google.com>
2024-08-09 17:38:12 +0530

    adding provider

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,7 @@
 module "main" {
-  source           = "../../root"
-  tfe_org          = var.tfe_org
-  ghe_oauth_token  = var.ghe_oauth_token
-  modules_registry = var.modules_registry
+  source              = "../../root"
+  tfe_org             = var.tfe_org
+  ghe_oauth_token     = var.ghe_oauth_token
+  tfe_teams_api_token = var.tfe_teams_api_token
+  modules_registry    = var.modules_registry
 }
